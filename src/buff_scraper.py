889478a10df3e2e56dc2d2a96f93c92486700e1c
ayricky <marianoricardo97@gmail.com>
2023-04-04 03:10:32 -0400

    bug fixes

--- src/buff_scraper.py
+++ src/buff_scraper.py
@@ -1,16 +1,18 @@
 import argparse
+import asyncio
+import concurrent.futures
 import csv
+import itertools
 import sqlite3
 import threading
 import time
 import traceback
-import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from queue import Queue, PriorityQueue
-import asyncio
-import aiohttp
+from queue import PriorityQueue, Queue
 
+import aiohttp
 import yaml
+from aiohttp_socks import ProxyConnector
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from rich.columns import Columns
@@ -27,31 +29,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from aiohttp_socks import ProxyConnector
-import itertools
+
 
 class MaxRetriesExceeded(Exception):
     pass
 
+
 class WebDriverNotInitialized(Exception):
     pass
 
+
 def initialize_csv():
-    with open('output.csv', mode='w', newline='', encoding='utf-8') as output_file:
+    with open("output.csv", mode="w", newline="", encoding="utf-8") as output_file:
         writer = csv.writer(output_file)
-        writer.writerow([
-            'item_id',
-            'raw_name',
-            'name',
-            'wear',
-            'skin_line',
-            'drop_down_index',
-            'option_index',
-            'button_text',
-            'option_text',
-            'option_value',
-            'additional_options'
-        ])
+        writer.writerow(
+            [
+                "item_id",
+                "raw_name",
+                "name",
+                "wear",
+                "skin_line",
+                "drop_down_index",
+                "option_index",
+                "button_text",
+                "option_text",
+                "option_value",
+                "additional_options",
+            ]
+        )
+
 
 class Worker:
     worker_id_counter = itertools.count()  # Add a counter for unique worker IDs
@@ -72,14 +78,14 @@ class Worker:
 
     def init_webdriver(self, proxy):
         options = webdriver.ChromeOptions()
-        options.add_argument(f'user-agent={UserAgent().random}')
+        options.add_argument(f"user-agent={UserAgent().random}")
         proxy_config = f'{proxy["proxy_type"].lower()}://{proxy["proxy"]}'
-        options.add_argument(f'--proxy-server={proxy_config}')
+        options.add_argument(f"--proxy-server={proxy_config}")
         options.add_argument("--log-level=3")
         options.add_argument("--headless")
 
         # Add the following line to suppress DevTools messages
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
         service = Service(executable_path="C:\\Users\\Ricky\\lab\\chromedriver_win32\\chromedriver.exe")
         driver = webdriver.Chrome(service=service, options=options)
@@ -89,7 +95,7 @@ class Worker:
     def scrape_data(self, task):
         if self.webdriver is None:
             raise WebDriverNotInitialized("Webdriver not initialized")
-        self.current_task_id = task['id']
+        self.current_task_id = task["id"]
         self.is_working = True
         url = f"https://buff.163.com/goods/{task['id']}"
         self.webdriver.get(url)
@@ -111,23 +117,25 @@ class Worker:
                     option_value = option["value"]
                     option_text = option.text
                     additional_options = []
-                    for p in option.find_next_siblings('div'):
-                        for value in p.select('p'):
+                    for p in option.find_next_siblings("div"):
+                        for value in p.select("p"):
                             additional_options.append(value.text)
 
-                    return_data.append({
-                        "item_id": task['id'],
-                        "name": task['name'],
-                        "raw_name": task['raw_name'],
-                        "wear": task['wear'],
-                        "skin_line": task['skin_line'],
-                        "drop_down_index": i,
-                        "option_index": j,
-                        "button_text": button_text,
-                        "option_text": option_text,
-                        "option_value": option_value,
-                        "additional_options": additional_options
-                    })
+                    return_data.append(
+                        {
+                            "item_id": task["id"],
+                            "name": task["name"],
+                            "raw_name": task["raw_name"],
+                            "wear": task["wear"],
+                            "skin_line": task["skin_line"],
+                            "drop_down_index": i,
+                            "option_index": j,
+                            "button_text": button_text,
+                            "option_text": option_text,
+                            "option_value": option_value,
+                            "additional_options": additional_options,
+                        }
+                    )
 
             time.sleep(2)
 
@@ -144,6 +152,7 @@ class Worker:
             except concurrent.futures.TimeoutError:
                 future.cancel()  # Cancel the task if it times out
                 raise
+
 
 class Scraper:
     def __init__(self, tasks, proxies, num_workers):
@@ -177,13 +186,12 @@ class Scraper:
             for task in tasks:
                 self.task_queue.put(task)  # Add the task to the task queue
 
-
     async def test_proxy(self, proxy_info, progress, task_id):
         url = "https://buff.163.com/"
         timeout = aiohttp.ClientTimeout(total=15)
 
-        proxy = proxy_info['proxy']
-        proxy_type = proxy_info['proxy_type']
+        proxy = proxy_info["proxy"]
+        proxy_type = proxy_info["proxy_type"]
         connector = ProxyConnector.from_url(f"{proxy_type}://{proxy}")
 
         async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
@@ -199,12 +207,14 @@ class Scraper:
     async def get_working_proxies(self, proxies):
         with Progress() as progress:
             test_proxy_task_id = progress.add_task("[cyan]Testing proxies...", total=len(proxies))
-            tasks = [asyncio.create_task(self.test_proxy(proxy_info, progress, test_proxy_task_id)) for proxy_info in proxies]
+            tasks = [
+                asyncio.create_task(self.test_proxy(proxy_info, progress, test_proxy_task_id)) for proxy_info in proxies
+            ]
             results = await asyncio.gather(*tasks)
 
         working_proxies = [proxy_info for proxy_info, is_working in zip(proxies, results) if is_working]
         return working_proxies
-    
+
     def start_scraping(self, dashboard, terminate_event):
         sentinel = object()  # Create a sentinel object to signal that there are no more tasks to process
 
@@ -235,22 +245,24 @@ class Scraper:
 
                     if scraped_data is not None:
                         with self.csv_write_lock:
-                            with open('output.csv', mode='a', newline='', encoding='utf-8') as output_file:
+                            with open("output.csv", mode="a", newline="", encoding="utf-8") as output_file:
                                 writer = csv.writer(output_file)
                                 for data in scraped_data:
-                                    writer.writerow([
-                                        data['item_id'],
-                                        data['name'],
-                                        data['raw_name'],
-                                        data['wear'],
-                                        data['skin_line'],
-                                        data['drop_down_index'],
-                                        data['option_index'],
-                                        data['button_text'],
-                                        data['option_text'],
-                                        data['option_value'],
-                                        data['additional_options']
-                                    ])
+                                    writer.writerow(
+                                        [
+                                            data["item_id"],
+                                            data["name"],
+                                            data["raw_name"],
+                                            data["wear"],
+                                            data["skin_line"],
+                                            data["drop_down_index"],
+                                            data["option_index"],
+                                            data["button_text"],
+                                            data["option_text"],
+                                            data["option_value"],
+                                            data["additional_options"],
+                                        ]
+                                    )
 
                         worker.performance_score += 1
                         dashboard.update_progress(dashboard.completed_tasks + 1)
@@ -281,7 +293,9 @@ class Scraper:
                                 self.workers.remove(worker)
                                 worker = new_worker
                             else:
-                                self.working_proxies = self.working_proxies[1:]  # Remove the proxy from the list if it doesn't work
+                                self.working_proxies = self.working_proxies[
+                                    1:
+                                ]  # Remove the proxy from the list if it doesn't work
                             self.worker_queue.put((-worker.performance_score, worker))
                         except IndexError:
                             # No more proxies left
@@ -338,9 +352,13 @@ class Dashboard:
         progress = f"{self.completed_tasks}/{self.total_tasks}"
         tasks_in_queue = self.total_tasks - self.completed_tasks
 
-        elapsed_time_text = Text(f"Time Elapsed: {time.strftime('%H:%M:%S', time.gmtime(self.elapsed_time))}", style="bold yellow")
+        elapsed_time_text = Text(
+            f"Time Elapsed: {time.strftime('%H:%M:%S', time.gmtime(self.elapsed_time))}", style="bold yellow"
+        )
         if self.estimated_time_remaining:
-            eta_text = Text(f"ETA: {time.strftime('%H:%M:%S', time.gmtime(self.estimated_time_remaining))}", style="bold yellow")
+            eta_text = Text(
+                f"ETA: {time.strftime('%H:%M:%S', time.gmtime(self.estimated_time_remaining))}", style="bold yellow"
+            )
         else:
             eta_text = Text("ETA: N/A", style="bold yellow")
 
@@ -361,7 +379,6 @@ class Dashboard:
             task_id = str(worker.current_task_id) if worker.current_task_id else "N/A"
             self.table.add_row(str(worker.worker_id), status, task_id, str(worker.performance_score))
 
-
     def update_progress(self, completed_tasks):
         self.completed_tasks = completed_tasks
 
@@ -372,12 +389,13 @@ class Dashboard:
                 live.update(Panel(self.table))
                 time.sleep(1)
 
+
 def main():
     parser = argparse.ArgumentParser(description="Web scraper with multiple workers.")
-    parser.add_argument('-n', '--num_workers', type=int, default=5, help="Number of workers to use for scraping.")
+    parser.add_argument("-n", "--num_workers", type=int, default=5, help="Number of workers to use for scraping.")
     args = parser.parse_args()
 
-    with open('working_proxies.yaml') as proxies_file:
+    with open("working_proxies.yaml") as proxies_file:
         proxies = yaml.safe_load(proxies_file)
 
     conn = sqlite3.connect("csgo_items.db")
@@ -392,8 +410,7 @@ def main():
     items = cur.fetchall()
     initialize_csv()
     tasks = [
-        {'id': item[0], 'raw_name': item[1], 'name': item[2], 'wear': item[3], 'skin_line': item[4]}
-        for item in items
+        {"id": item[0], "raw_name": item[1], "name": item[2], "wear": item[3], "skin_line": item[4]} for item in items
     ]
 
     num_workers = args.num_workers
@@ -415,5 +432,6 @@ def main():
     scraper.close_all_drivers()
     conn.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
